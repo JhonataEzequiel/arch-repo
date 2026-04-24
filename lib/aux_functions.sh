603cc08d0a932ce9e533ad5cefeadae62ca0fec9
JhonataEzequiel <jhonataezequiel@hotmail.com>
2026-04-24 01:37:41 -0300

    fix select

--- lib/aux_functions.sh
+++ lib/aux_functions.sh
@@ -5,31 +5,30 @@ multi_select() {
     local selected=()
     local cursor=0
     local key
- 
+
     for (( i=0; i<${#options[@]}; i++ )); do
         selected+=(false)
     done
- 
+
     tput civis
+
+    # Initial draw
+    for (( i=0; i<${#options[@]}; i++ )); do
+        if [[ "${selected[$i]}" == true ]]; then
+            local mark="[x]"
+        else
+            local mark="[ ]"
+        fi
+        if [[ $i -eq $cursor ]]; then
+            echo -e "\e[7m  $mark ${options[$i]}\e[0m"
+        else
+            echo "  $mark ${options[$i]}"
+        fi
+    done
+    echo ""
+    echo "  [space] select/deselect   [enter] confirm   [↑↓] navigate"
+
     while true; do
-        tput cup "$(tput lines)" 0
-        for (( i=0; i<${#options[@]}; i++ )); do
-            tput el
-            if [[ "${selected[$i]}" == true ]]; then
-                local mark="[x]"
-            else
-                local mark="[ ]"
-            fi
-            if [[ $i -eq $cursor ]]; then
-                echo -e "\e[7m  $mark ${options[$i]}\e[0m"
-            else
-                echo "  $mark ${options[$i]}"
-            fi
-        done
-        echo ""
-        tput el
-        echo "  [space] select/deselect   [enter] confirm   [↑↓] navigate"
- 
         IFS= read -rsn1 key
         if [[ $key == $'\x1b' ]]; then
             read -rsn2 -t 0.1 key
@@ -46,10 +45,27 @@ multi_select() {
         elif [[ $key == '' ]]; then
             break
         fi
- 
+
+        # Redraw
         tput cuu $(( ${#options[@]} + 2 ))
+        for (( i=0; i<${#options[@]}; i++ )); do
+            tput el
+            if [[ "${selected[$i]}" == true ]]; then
+                local mark="[x]"
+            else
+                local mark="[ ]"
+            fi
+            if [[ $i -eq $cursor ]]; then
+                echo -e "\e[7m  $mark ${options[$i]}\e[0m"
+            else
+                echo "  $mark ${options[$i]}"
+            fi
+        done
+        echo ""
+        tput el
+        echo "  [space] select/deselect   [enter] confirm   [↑↓] navigate"
     done
- 
+
     tput cnorm
     _result=()
     for (( i=0; i<${#options[@]}; i++ )); do
@@ -63,10 +79,34 @@ single_select() {
     local options=("$@")
     local cursor=0
     local key
- 
+
     tput civis
+
+    # Initial draw
+    for (( i=0; i<${#options[@]}; i++ )); do
+        if [[ $i -eq $cursor ]]; then
+            echo -e "\e[7m  ${options[$i]}\e[0m"
+        else
+            echo "  ${options[$i]}"
+        fi
+    done
+    echo ""
+    echo "  [enter] confirm   [↑↓] navigate"
+
     while true; do
-        tput cup "$(tput lines)" 0
+        IFS= read -rsn1 key
+        if [[ $key == $'\x1b' ]]; then
+            read -rsn2 -t 0.1 key
+            case $key in
+                '[A') (( cursor > 0 )) && (( cursor-- )) ;;
+                '[B') (( cursor < ${#options[@]} - 1 )) && (( cursor++ )) ;;
+            esac
+        elif [[ $key == '' ]]; then
+            break
+        fi
+
+        # Redraw
+        tput cuu $(( ${#options[@]} + 2 ))
         for (( i=0; i<${#options[@]}; i++ )); do
             tput el
             if [[ $i -eq $cursor ]]; then
@@ -78,21 +118,8 @@ single_select() {
         echo ""
         tput el
         echo "  [enter] confirm   [↑↓] navigate"
- 
-        IFS= read -rsn1 key
-        if [[ $key == $'\x1b' ]]; then
-            read -rsn2 -t 0.1 key
-            case $key in
-                '[A') (( cursor > 0 )) && (( cursor-- )) ;;
-                '[B') (( cursor < ${#options[@]} - 1 )) && (( cursor++ )) ;;
-            esac
-        elif [[ $key == '' ]]; then
-            break
-        fi
- 
-        tput cuu $(( ${#options[@]} + 2 ))
     done
- 
+
     tput cnorm
     _result="${options[$cursor]}"
 }
